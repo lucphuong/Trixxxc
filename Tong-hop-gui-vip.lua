613830e68fe98f55362cbc37b4def64a3d82cae6
lucphuong <lucthiphuongpltn@gmail.com>
2025-11-16 19:28:47 +0700

    Update Tong-hop-gui-vip.lua

--- Tong-hop-gui-vip.lua
+++ Tong-hop-gui-vip.lua
@@ -1,1 +1,7 @@
-game:GetService("Players").LocalPlayer:Kick("Script đã update truy cập tiktok @phnglcth7 để tìm bản update mới nhất của soliar tổng hợp")
+if setclipboard then
+    setclipboard("@phnglcth7")
+end
+
+task.wait(0.5)
+
+game:GetService("Players").LocalPlayer:Kick("Script đã đc update truy cập tiktok @phnglcth7 để tìm script mới nhất")
